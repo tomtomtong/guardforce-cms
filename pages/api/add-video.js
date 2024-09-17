cc2fee552324c746ab9fb38f121436d8bacc07ec
Softmind Tom <tomtomtongtong@gmail.com>
2024-09-17 08:39:25 +0800

    latest

--- pages/api/add-video.js
+++ pages/api/add-video.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
   try {
     const getResult = await s3.getObject({
       Bucket: bucket,
-      Key: 'database.json', // Assuming this is the key for your database file
+      Key: 'db.json', // Assuming this is the key for your database file
     }).promise();
     dbContent = JSON.parse(getResult.Body.toString('utf-8'));
   } catch (e) {
